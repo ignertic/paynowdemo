079d01bb108d6228cf52b7f70d1f3d401ce0c570
SuperCode <ignertic@icloud.com>
2020-05-06 14:51:47 +0200

    fix

--- index1.php
+++ index1.php
@@ -1,7 +1,7 @@
 <?php 
 
 require_once 'paynow/autoloader.php';
-$host = 'supercode.co.zw';
+$host = 'localhost';
 $db = 'fkdmqrpy_paynow';
 $username = 'fkdmqrpy_super';
 $password = '9g00isho9Gisho075';
