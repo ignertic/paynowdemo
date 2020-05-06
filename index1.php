a15c6d3783b194440509dbef7e95f4a10f6de70d
SuperCode <ignertic@icloud.com>
2020-05-06 14:06:44 +0200

    init

---
+++ index1.php
@@ -0,0 +1,51 @@
+<?php 
+
+require_once 'C:/Users/acer/Desktop/paynow/paynow/autoloader.php';
+$host = 'localhost';
+$db = 'paynow';
+$username = 'supercode';
+$password = '9g00isho';
+
+// require_once 'dbconfig.php';
+ 
+$dsn= "mysql:host=" . $host . ";dbname=" . $db;
+ 
+try{
+ // create a PDO connection with the configuration data
+ $conn = new PDO($dsn, $username, $password);
+ // display a message if connected to database successfully
+ if($conn){
+ echo "Connected to the <strong>$db</strong> database successfully!";
+        }
+}catch (PDOException $e){
+ // report error message
+ echo $e->getMessage();
+}
+
+$email = $_GET['email'];
+$amount = $_GET['amount'];
+// var_dump($email);
+
+
+$paynow = new Paynow\Payments\Paynow(
+    '6054',
+    '960ad10a-fc0c-403b-af14-e9520a50fbf4',
+    'http://localhost/success.php?email=' . $email,
+    'http://localhost/payment.php',
+    
+);
+
+$payment = $paynow->createPayment('Invoice', $email);
+$payment->add('Bananas', floatval($amount));
+// $payment->add('Apples', 3.40);
+$response = $paynow->send($payment);
+$poll = $response->pollUrl();
+$status = 'unknown';
+$conn->query("INSERT INTO transactions (email, poll, status) VALUES ('" . $email . "', '" . $poll . "', '" . $status . "')");
+$url = 'Location: ' . $response->redirectUrl(); // http://www.example.com/another-page.php'
+// add poll url to databse with email key
+
+header($url);
+// var_dump($response);
+
+?>
---
+++ success.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once 'C:/Users/acer/Desktop/paynow/paynow/autoloader.php';
+
+$host = 'localhost';
+$db = 'paynow';
+$username = 'supercode';
+$password = '9g00isho';
+$email = $_GET['email'];
+
+$paynow = new Paynow\Payments\Paynow(
+    '6054',
+    '960ad10a-fc0c-403b-af14-e9520a50fbf4',
+    'http://localhost/success.php?email=' . $email,
+    'http://localhost/payment.php',
+    
+);
+// require_once 'dbconfig.php';
+ 
+$dsn= "mysql:host=" . $host . ";dbname=" . $db;
+ 
+try{
+ // create a PDO connection with the configuration data
+ $conn = new PDO($dsn, $username, $password);
+ // display a message if connected to database successfully
+ if($conn){
+ // echo "Connected to the <strong>$db</strong> database successfully!";
+        }
+}catch (PDOException $e){
+ // report error message
+ echo $e->getMessage();
+}
+
+$email = $_GET['email'];
+// select from db and check 
+$query = $conn->query("SELECT poll FROM transactions WHERE email='" . $email . "'");
+$data = $query->fetch();
+// header('Location: ' . $data['poll']);
+// check status
+$stat = $paynow->pollTransaction($data['poll']);
+$status=$stat->status();
+if ($status=="cancelled"){
+	echo "<h1>I'm guessing you're temporarily broke :(<h1>";
+	// sleep(5);
+	// header('Location: http://localhost');
+	
+}else{
+	echo "<h1> Thank You For Paying</h1>";
+	// sleep(5);
+	// header('Location: http://localhost');
+}
+
+?>
